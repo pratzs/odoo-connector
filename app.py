3cdadf0f7caada772785c9168f9fc2bb5e5372b8
pratzs <pratzs@live.com>
2026-01-20 14:55:15 +1300

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ from services.products import (
     sync_products_master, 
     process_product_data, 
     archive_shopify_duplicates, 
-    cleanup_shopify_products
+    cleanup_shopify_products,
     find_shopify_product_by_sku
 )
 from services.customers import sync_customers_master
